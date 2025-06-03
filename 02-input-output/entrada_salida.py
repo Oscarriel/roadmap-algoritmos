859edd483d1a9b3b9b5f1cfc19566073af80a018
Oscar Carriel <oscar.carriel@virginiogomez.cl>
2025-06-03 18:35:58 -0400

    Roadmap revisado

--- 02-input-output/entrada_salida.py
+++ 02-input-output/entrada_salida.py
@@ -4,22 +4,27 @@
 # Paso 1: Pedir datos al usuario con la función input()
 # Ejemplo: Pedir el nombre del usuario.
 # nombre = input("¿Cómo te llamas? ")
+nombre = input("¿Como te llamas? :")
 
 # Paso 2: Convertir los datos ingresados a números si es necesario.
 # Ejemplo: Convertir un dato ingresado a entero con int() o a decimal con float().
 # edad = int(input("¿Cuántos años tienes? "))
 # altura = float(input("¿Cuánto mides en metros? "))
+edad = int(input("Dame tu edad: "))
+altura = float(input("Dame tu altura en metros: "))
 
 # Paso 3: Mostrar mensajes personalizados al usuario.
 # Hay tres formas comunes de concatenar texto y variables en Python:
 
 # 3.1 Usando comas (,) en la función print().
 # Ejemplo: print("Hola,", nombre)
+print("Hola,", nombre)
 
 # 3.2 Usando el operador + (requiere convertir números a texto con str()).
 # Ejemplo: print("Hola, " + nombre)
+print("Hola, " + nombre)
 
 # 3.3 Usando f-strings (requiere Python 3.6 o superior).
 # Ejemplo: print(f"Hola, {nombre}")
-
+print(f"Hola, {nombre}")
 # Ahora completa cada paso con tu propio código.
